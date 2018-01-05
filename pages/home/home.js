5c4e163cdf6f5ee705753b37dd6c4c84b4541fd9
CDX <cdxofemail@163.com>
2018-01-05 17:58:53 +0800

    update home / banner

--- pages/home/home.js
+++ pages/home/home.js
@@ -5,9 +5,18 @@ Page({
    * 页面的初始数据
    */
   data: {
-  
+    slider: [
+      { picUrl: 'http://y.gtimg.cn/music/photo_new/T003R720x288M000000rVobR3xG73f.jpg' },
+      { picUrl: 'http://y.gtimg.cn/music/photo_new/T003R720x288M000000j6Tax0WLWhD.jpg' },
+      { picUrl: 'http://y.gtimg.cn/music/photo_new/T003R720x288M000000a4LLK2VXxvj.jpg' }
+    ],
+    swiperCurrent: 0,
   },
-
+  swiperChange: function (e) {
+    this.setData({
+      swiperCurrent: e.detail.current
+    })
+  },
   /**
    * 生命周期函数--监听页面加载
    */
